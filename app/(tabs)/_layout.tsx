b9376558147482b8f021ff848e4676754bc85def
Padda Rohit Kumar <rohitkumarsoftware@gmail.com>
2024-11-08 10:12:17 +0530

    remove About and Index screens, add ImageViewer and button components

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,50 @@
 import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
 	return (
-		<Tabs>
-			<Tabs.Screen name='index' options={{ title: 'Home' }} />
-			<Tabs.Screen name='about' options={{ title: 'About' }} />
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: '#ffd33d',
+				headerStyle: {
+					backgroundColor: '#25292e',
+				},
+				headerShadowVisible: false,
+				headerTintColor: '#fff',
+				tabBarStyle: {
+					backgroundColor: '#25292e',
+				},
+			}}
+		>
+			<Tabs.Screen
+				name='index'
+				options={{
+					title: 'Home',
+					headerTitle: 'Sticker Smash',
+					tabBarIcon: ({ focused, color }) => (
+						<Ionicons
+							name={focused ? 'home-sharp' : 'home-outline'}
+							size={30}
+							color={color}
+						/>
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name='about'
+				options={{
+					headerTitle: 'About',
+					tabBarIcon: ({ focused, color }) => (
+						<Ionicons
+							name={
+								focused ? 'information-circle' : 'information-circle-outline'
+							}
+							color={color}
+							size={30}
+						/>
+					),
+				}}
+			/>
 		</Tabs>
 	);
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,78 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { Link } from 'expo-router';
+import { View, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
+import Button from '@/components/button';
+import ImageViewer from '@/components/ImageViewer';
+import CircleButton from '@/components/CircleButton';
+import IconButton from '@/components/IconButton';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
+	const [selectedImage, setSelectedImage] = useState<string | undefined>(
+		undefined
+	);
+	const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+	const pickImageAsync = async () => {
+		let result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ImagePicker.MediaTypeOptions.Images,
+			allowsEditing: true,
+			quality: 1,
+		});
+
+		if (!result.canceled) {
+			setSelectedImage(result.assets[0].uri);
+			setShowAppOptions(true);
+		} else {
+			alert('You did not select any image.');
+		}
+	};
+
+	const onReset = () => {
+		setShowAppOptions(false);
+	};
+
+	const onAddSticker = () => {
+		alert('Add sticker');
+	};
+
+	const onSaveImageAsync = () => {
+		alert('Saved Image');
+	};
+
 	return (
 		<View style={styles.container}>
-			<Text style={styles.text}>Home screen</Text>
-			<Link href='/about' style={styles.button}>
-				Go to About
-			</Link>
+			<View style={styles.imageContainer}>
+				<ImageViewer
+					imgSource={PlaceholderImage}
+					selectedImage={selectedImage}
+				/>
+			</View>
+			{showAppOptions ? (
+				<View style={styles.optionContainer}>
+					<View style={styles.optionRow}>
+						<IconButton icon='refresh' label='Refresh' onPress={onReset} />
+						<CircleButton onPress={onAddSticker} />
+						<IconButton
+							icon='save-alt'
+							label='Save'
+							onPress={onSaveImageAsync}
+						/>
+					</View>
+				</View>
+			) : (
+				<View style={styles.footerContainer}>
+					<Button
+						theme='primary'
+						label='Choose a photo'
+						onPress={pickImageAsync}
+					/>
+					<Button
+						label='Use this photo'
+						onPress={() => setShowAppOptions(true)}
+					/>
+				</View>
+			)}
 		</View>
 	);
 }
@@ -17,14 +82,20 @@ const styles = StyleSheet.create({
 		flex: 1,
 		backgroundColor: '#25292e',
 		alignItems: 'center',
-		justifyContent: 'center',
 	},
-	text: {
-		color: '#fff',
+	imageContainer: {
+		flex: 1,
 	},
-	button: {
-		fontSize: 20,
-		textDecorationLine: 'underline',
-		color: '#fff',
+	footerContainer: {
+		flex: 1 / 3,
+		alignItems: 'center',
+	},
+	optionContainer: {
+		position: 'absolute',
+		bottom: 80,
+	},
+	optionRow: {
+		alignItems: 'center',
+		flexDirection: 'row',
 	},
 });
